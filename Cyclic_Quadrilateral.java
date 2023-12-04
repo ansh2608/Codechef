e1d72b64c023f79ffdd29190439edea9bdd7cd23
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2023-12-04 17:26:49 +0530

    Add files via upload

--- Cyclic_Quadrilateral.java
+++ Cyclic_Quadrilateral.java
@@ -1,13 +1,22 @@
-class Solution {
-    public int maxCoins(int[] piles) {
-        Arrays.sort(piles);
-        int ans = 0;
-        int n = piles.length;
+import java.util.Scanner;
 
-        for (int i = n / 3; i < n; i += 2) {
-            ans += piles[i];
+public class Cyclic_Quadrilateral {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for(int i = 0; i<t; i++)
+        {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            int d = scan.nextInt();
+            if(a+c==180 && b+d==180)
+            {
+                System.out.println("yes");
+            }
+            else{
+                System.out.println("no");
+            }
         }
-
-        return ans;
     }
 }
