c24f098ee08ee2312747c86b787cf1fa2a599edd
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2023-12-07 16:21:49 +0530

    Add files via upload

--- Presents_for_Cheffina.java
+++ Presents_for_Cheffina.java
@@ -1,182 +1,19 @@
-
-class Solution
-
-{
-
-    public:
-
-    //Function to detect cycle using DSU in an undirected graph.
-
- vector<int> parent;
-
-    
-
-    vector<int> rank;
-
-    
-
-    int find(int x)
-
-    {
-
-        if(parent[x] == x)
-
-        {
-
-            return x;
-
-        }
-
-        
-
-        parent[x] = find(parent[x]);
-
-        
-
-        return parent[x];
-
-    }
-
-    
-
-    void union1(int x, int y)
-
-    {
-
-        int x_rep = find(x);
-
-        
-
-        int y_rep = find(y);
-
-        
-
-        if(x_rep == y_rep)
-
-        {
-
-            return;
-
-        }
-
-        
-
-        if(rank[x_rep] < rank[y_rep])
-
-        {
-
-            parent[x_rep] = y_rep;
-
-        }
-
-        
-
-        else if(rank[x_rep] > rank[y_rep])
-
-        {
-
-            parent[y_rep] = x_rep;
-
-        }
-
-        
-
-        else
-
-        {
-
-            parent[y_rep] = x_rep;
-
-            
-
-            rank[x_rep]++;
-
-        }
-
-    }
-
-    
-
-    int detectCycle(int V, vector<int>adj[])
-
-    {
-
-        parent.resize(V);
-
-        
-
-        rank.resize(V);
-
-        
-
-        for(int i = 0; i < V; i++)
-
-        {
-
-            parent[i] = i;
-
-            
-
-            rank[i] = 0;
-
-        }
-
-        
-
-        set<pair<int, int>> s;
-
-        
-
-        for(int v = 0; v < V; v++)
-
-        {
-
-            for(auto u : adj[v])
-
-            {
-
-                if(s.find({u, v}) != s.end() or s.find({v, u}) != s.end())
-
-                {
-
-                    continue;
-
-                }
-
-                
-
-                s.insert({u, v});
-
-                
-
-                int u_rep = find(u);
-
-                
-
-                int v_rep = find(v);
-
-                
-
-                if(u_rep == v_rep)
-
-                {
-
-                    return true;
-
-                }
-
-                
-
-                union1(u, v);
-
+import java.util.Scanner;
+
+public class Presents_for_Cheffina {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) {
+            int n=sc.nextInt();
+            if(n%5==0) {
+                int free =n/5;
+                System.out.println(n-free);
             }
-
+            else {
+                int free= n/5;
+                System.out.println(n-free);
+            }
         }
-
-        
-
-        return false;
-
+    }
 }
-
-};
