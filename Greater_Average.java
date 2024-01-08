6cf43d229524c578d31fe71e3bcbbf040ca69d4d
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-01-08 14:48:56 +0530

    Add files via upload

---
+++ Greater_Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Greater_Average {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t--!=0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            float ans=(float) (a+b)/2;
+            if (ans > c) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
