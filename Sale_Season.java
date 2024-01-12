2c8141da282784825cbe609e05c5fcaf929f6970
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-01-12 12:16:02 +0530

    Add files via upload

---
+++ Sale_Season.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sale_Season {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t--!=0){
+            int x=sc.nextInt();
+            if (x<=100) System.out.println(x);
+            else if (x>100 && x<=1000) System.out.println(x-25);
+            else if (x>1000 && x<=5000) System.out.println(x-100);
+            else System.out.println(x-500);
+        }
+    }
+}
