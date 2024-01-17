298a78a86b162f75b5a82d2be1e9e0af8178e134
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-01-17 12:36:26 +0530

    Add files via upload

---
+++ Monopoly.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Monopoly {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t--!=0){
+            int[] arr=new int[4];
+            for (int i=0;i<4;i++) arr[i]=sc.nextInt();
+            Arrays.sort(arr);
+            if (arr[0]+arr[1]+arr[2]<arr[3]) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
