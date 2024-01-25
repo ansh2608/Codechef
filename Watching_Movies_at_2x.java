a2029f5f7ce14bd2f4c145c2568f3286e28b3b3a
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-01-25 20:58:35 +0530

    Add files via upload

---
+++ Watching_Movies_at_2x.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Watching_Movies_at_2x {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int m=s.nextInt();
+        System.out.println(n-m/2);
+    }
+}
