72626f4a83e9a06e8c9430718bf09f7b4fa3d57d
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-05 21:29:34 +0530

    Add files via upload

--- Chef_and_his_Apps.java
+++ Chef_and_his_Apps.java
@@ -1,10 +1,17 @@
-public class Solution {
-    public int hammingWeight(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
+import java.util.Scanner;
+
+public class Chef_and_his_Apps {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t--!=0){
+            int s=sc.nextInt();
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            if (x+y+z<=s) System.out.println(0);
+            else if (x+z<=s || y+z<=s) System.out.println(1);
+            else System.out.println(2);
         }
-        return count;
     }
 }
