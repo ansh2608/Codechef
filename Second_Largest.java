6525bbd1b19d93ec61aa1ed4496b09bdc6284a36
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-10 19:38:56 +0530

    Add files via upload

---
+++ Second_Largest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Second_Largest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t--!=0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c= sc.nextInt();
+            if (a>=b && a>=c) System.out.println(Math.min(a,Math.max(b,c)));
+            else if (b>=a && b>=c) System.out.println(Math.min(b,Math.max(a,c)));
+            else if (c>=a && c>=b) System.out.println(Math.min(c,Math.max(a,b)));
+        }
+    }
+}
