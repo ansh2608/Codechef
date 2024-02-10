92d38d48bc8e01a1a9a0d86b3c8a3a9eff01e83f
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-10 19:38:23 +0530

    Add files via upload

--- Pass_or_Fail.java
+++ Pass_or_Fail.java
@@ -3,24 +3,14 @@ import java.util.Scanner;
 public class Pass_or_Fail {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        for(int i=1;i<=t;i++)
-        {
+        int t= sc.nextInt();
+        while (t--!=0){
             int n=sc.nextInt();
             int x=sc.nextInt();
             int p=sc.nextInt();
-            int marks=x*3;
-            int ic=n-x;
-            int re=marks-ic;
-            if(re>=p)
-            {
-                System.out.println("PASS");
-            }
-            else
-            {
-                System.out.println("FAIL");
-            }
-
+            int numbers=3*x-(n-x);
+            if (numbers>=p) System.out.println("PASS");
+            else System.out.println("FAIL");
         }
     }
 }
