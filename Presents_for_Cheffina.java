409b465c1c49f72a28374c36f6f5d15c23f2d373
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-14 22:08:53 +0530

    Add files via upload

--- Presents_for_Cheffina.java
+++ Presents_for_Cheffina.java
@@ -4,16 +4,11 @@ public class Presents_for_Cheffina {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        while(t-->0) {
+        while (t--!=0){
             int n=sc.nextInt();
-            if(n%5==0) {
-                int free =n/5;
-                System.out.println(n-free);
-            }
-            else {
-                int free= n/5;
-                System.out.println(n-free);
-            }
+            int fac=1;
+            for (int i=1;i<=n;i++) fac*=i;
+            System.out.println(fac);
         }
     }
 }
