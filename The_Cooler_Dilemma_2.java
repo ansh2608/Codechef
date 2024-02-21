51846de06e86bb3d6badf516fa391cb4cf4b7874
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-21 22:24:22 +0530

    Add files via upload

---
+++ The_Cooler_Dilemma_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class The_Cooler_Dilemma_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+        while (t != 0) {
+            Long x = sc.nextLong();
+            Long y = sc.nextLong();
+            if ( y % x != 0) System.out.println(y / x);
+            else System.out.println((y/x)-1);
+            t--;
+        }
+    }
+}
