d73c6949e00239db79a6c71b739cba20ca23f7f6
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-04-25 21:50:13 +0530

    Add files via upload

---
+++ Coins_And_Triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Coins_And_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T>0){
+            int N = sc.nextInt();
+            int height = 0;
+            while(((height*(height+1))/2) < N) height = height + 1;
+            if((height*(height+1))/2 == N) System.out.println(height);
+            else System.out.println(height-1);
+            T--;
+        }
+    }
+}
