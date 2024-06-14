43533c48b1ff941d71e5f7a4be806fad6f91a503
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-14 09:42:07 +0530

    Add files via upload

---
+++ Luigi_and_Uniformity.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Luigi_and_Uniformity {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++)arr[i]=sc.nextInt();
+            if(n==1){
+                System.out.println(0);
+                continue;
+            }
+            int min=Integer.MAX_VALUE;
+            for(int num:arr)min=Math.min(min,num);
+            boolean flag=true;
+            int count=0;
+            for(int i=0;i<n;i++){
+                if(arr[i]%min!=0){
+                    flag=false;
+                    break;
+                }
+                if(arr[i]==min)count++;
+            }
+            if(flag)System.out.println(n-count);
+            else System.out.println(n);
+        }
+    }
+}
