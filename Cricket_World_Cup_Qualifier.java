d0db0b8beac255dff37356e0a4584e23add1479b
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-21 15:56:57 +0530

    Add files via upload

---
+++ Cricket_World_Cup_Qualifier.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Cricket_World_Cup_Qualifier {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner read = new Scanner(System.in);
+        int X = read.nextInt();
+        if(X>=12) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
