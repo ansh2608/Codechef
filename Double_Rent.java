e76efbd6551709e05e5c7386e3cf8f5975584615
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-24 20:51:02 +0530

    Add files via upload

---
+++ Double_Rent.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Double_Rent {
+    public static void main(String[] args) {
+        Scanner inirent = new Scanner(System.in);
+        int X = inirent.nextInt();
+        System.out.println(X * 2);
+    }
+}
