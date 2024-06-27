65c5b47ce1196f5c6e975f8f71bf4f82de67b9fd
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-27 18:47:06 +0530

    Add files via upload

---
+++ Biryani_classes.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Biryani_classes {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=0;i<n;i++){
+            int a=s.nextInt();
+            int b=s.nextInt();
+            System.out.println(a*b);
+        }
+    }
+}
