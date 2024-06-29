2b5ff94eaea6d055be3011d07e5a701e86f75423
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-29 12:35:06 +0530

    Add files via upload

---
+++ How_many_unattempted_problems.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class How_many_unattempted_problems {
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner inp = new Scanner(System.in);
+        int a=inp.nextInt();
+        int b=inp.nextInt();
+        System.out.println(a-b);
+    }
+}
