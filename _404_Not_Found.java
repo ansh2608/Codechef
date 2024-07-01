39b26edae559955b021c1836f8958e22fa3563cb
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-07-01 12:36:47 +0530

    Add files via upload

---
+++ _404_Not_Found.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _404_Not_Found {
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner in=new Scanner(System.in);
+        int X=in.nextInt();
+        if(X==404) System.out.println("Not Found");
+        else System.out.println("Found");
+    }
+}
