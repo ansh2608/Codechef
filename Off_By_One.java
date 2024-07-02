1ddeb4e042785a004fe2f4cea23b89561f1e05a2
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-07-02 10:55:23 +0530

    Add files via upload

---
+++ Off_By_One.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Off_By_One {
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((a+b) + "1");
+    }
+}
