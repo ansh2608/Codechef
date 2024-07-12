92e7830e83f489b69c12556865a27059221f3019
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-07-12 11:33:30 +0530

    Add files via upload

--- Best_of_Two.java
+++ Best_of_Two.java
@@ -1,23 +1,13 @@
 import java.util.Scanner;
 
 public class Best_of_Two {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while (t--!=0){
-            int a1=sc.nextInt();
-            int a2=sc.nextInt();
-            int a3=sc.nextInt();
-            int b1=sc.nextInt();
-            int b2=sc.nextInt();
-            int b3=sc.nextInt();
-            int a = Math.min(a1, Math.min(a2, a3));
-            int b = Math.min(b1, Math.min(b2, b3));
-            int x = a1 + a2 + a3 - a;
-            int y = b1 + b2 + b3 - b;
-            if(x>y) System.out.println("Alice");
-            else if(x<y) System.out.println("Bob");
-            else System.out.println("Tie");
+    public static void main(String args[]) {
+        Scanner inp = new Scanner(System.in);
+        int t = inp.nextInt();
+        while( t-- > 0 ) {
+            int x = inp.nextInt();
+            int y = inp.nextInt();
+            System.out.println( Math.max( x, y) );
         }
     }
 }
