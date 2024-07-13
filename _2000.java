299ee0e7f08a56e1ef908854cb278766bb5b6159
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-07-13 16:52:46 +0530

    Add files via upload

---
+++ _2000.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class _2000 {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        System.out.println(4*n);
+    }
+}
