8eab5f562eef557af589a054b8c8afdcf9439c5f
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-07-19 15:19:16 +0530

    Add files via upload

---
+++ Counting_words.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Counting_words {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(y*x);
+        }
+    }
+}
