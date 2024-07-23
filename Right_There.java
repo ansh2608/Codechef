012c7c9f3ba440ac40e8af469fd28649d7c20e63
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-07-23 18:54:11 +0530

    Add files via upload

---
+++ Right_There.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Right_There {
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int N=sc.nextInt();
+            int X=sc.nextInt();
+            if(N<=X)
+                System.out.println("yes");
+            else
+                System.out.println("No");
+        }
+    }
+}
