537f148a715312d89edfce0b08725fdd32fad1d4
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-08-14 19:14:15 +0530

    Add files via upload

---
+++ Fever.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Fever {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner s =new Scanner(System.in);
+        int n =s.nextInt();
+        while(n-->0){
+            int a = s.nextInt();
+            if(a>98) System.out.println("YES");
+            else System.out.println("No");
+        }
+    }
+}
