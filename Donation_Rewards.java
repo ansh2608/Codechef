deaa7e30c8de3e0557eb24e0f615928faf706274
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-02 18:46:01 +0530

    Add files via upload

---
+++ Donation_Rewards.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Donation_Rewards {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner num=new Scanner(System.in);
+        int n= num.nextInt();
+        while(n-->0) {
+            int m=num.nextInt();
+            if (m>6) System.out.println("gold");
+            else if(m<=3) System.out.println("bronze");
+            else System.out.println("silver");
+            }
+        }
+    }
+}
