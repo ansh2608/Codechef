c0c380385fd70925dedc055ef690b2bebbb43876
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-06 18:17:48 +0530

    Add files via upload

---
+++ Height_of_Rationals.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Height_of_Rationals {
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t,a,b,e;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c;
+        c=Math.max(a,b);
+        System.out.println(c);
+    }
+}
