8ee4b4105522c855469383ea8398f88db2566af7
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-19 10:30:38 +0530

    Add files via upload

---
+++ Maximum_Submissions.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Maximum_Submissions {
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        while(n-->0){
+            int a=s.nextInt();
+            System.out.println(2*a);
+        }
+    }
+}
