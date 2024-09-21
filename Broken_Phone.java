397563a4c27c973317a8dc4cf7a871d631f61336
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-21 10:14:12 +0530

    Add files via upload

---
+++ Broken_Phone.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Broken_Phone {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if(x < y) System.out.println("REPAIR");
+            else if (x > y) System.out.println("NEW PHONE");
+            else System.out.println("ANY");
+        }
+    }
+}
